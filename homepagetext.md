ab546f167b4f9d53d948d18e91c72f6b1c5029cf
Leander Berg <reg@lulebe.net>
2021-05-01 22:58:54 +0200

    markdown display

---
+++ homepagetext.md
@@ -0,0 +1,10 @@
+# AKO Bundestreffen
+
+## Programm
+
+1. Treffen
+2. Wieder gehen
+
+## Wo?
+
+_Dritte Straße links_
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
-module.exports = (req, res) => {
-  res.send()
+const marked = require('marked')
+const sanitize = require('sanitize-html')
+const fs = require('fs').promises
+const path = require('path')
+
+const tmpl = require.main.require('./templates')
+
+module.exports = async (req, res) => {
+  const md = await fs.readFile(path.join(global.appRoot, 'homepagetext.md'), 'utf-8')
+  const opts = {
+    markdownconvert: sanitize(marked(md))
+  }
+  tmpl.render('index.twig', opts).then(rendered => res.end(rendered))
 }
