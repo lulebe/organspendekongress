37b242d9680b8d8141a58a8a70d0c5d50100aa9a
Leander Berg <reg@lulebe.net>
2021-06-02 14:21:08 +0200

    Added user count

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,11 +1,12 @@
 const tmpl = require.main.require('./templates')
 const settings = require.main.require('./settings')
-const { Workshop } = require.main.require('./db')
+const { Workshop, User } = require.main.require('./db')
 
 module.exports = async (req, res) => {
   const opts = {
     workshops: await Workshop.findAll({order: [['slot', 'ASC'], ['title', 'ASC']]}),
-    workshopsopen: settings.get('workshopsopen')
+    workshopsopen: settings.get('workshopsopen'),
+    userCount: await User.count()
   }
   tmpl.render('admin/index.twig', opts).then(rendered => res.end(rendered))
 }
