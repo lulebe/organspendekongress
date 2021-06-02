759eb70dcdeac27007b9255a447d37a25bb2b432
Leander Berg <reg@lulebe.net>
2021-06-02 14:28:01 +0200

    Added free slots data

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -4,9 +4,10 @@ const { Workshop, User } = require.main.require('./db')
 
 module.exports = async (req, res) => {
   const opts = {
-    workshops: await Workshop.findAll({order: [['slot', 'ASC'], ['title', 'ASC']]}),
+    workshops: await Workshop.findAll({order: [['slot', 'ASC'], ['title', 'ASC']], include: [{model: User, attributes: ['id']}]}),
     workshopsopen: settings.get('workshopsopen'),
     userCount: await User.count()
   }
+  opts.workshops.forEach(ws -> ws.free = ws.maxPeople - ws.Users.length)
   tmpl.render('admin/index.twig', opts).then(rendered => res.end(rendered))
 }
